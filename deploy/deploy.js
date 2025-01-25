0923d6246d3e295efacf8538fe81b90742112e4d
Sudharaka Ashen <sudharakaashenaudi@icloud.com>
2025-01-25 06:02:03 +0530

    Initial commit

---
+++ deploy/deploy.js
@@ -0,0 +1,16 @@
+
+const hre = require("hardhat");
+
+async function main() {
+    const Manifestation = await hre.ethers.getContractFactory("Manifestation");
+    const manifestation = await Manifestation.deploy();
+    await manifestation.deployed();
+    console.log("Manifestation deployed to:", manifestation.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
